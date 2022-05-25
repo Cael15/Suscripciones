084faaa02c0e0376b2713d10cae731119817275c
cael15 <djdelgado17@gmail.com>
2022-05-25 10:36:18 -0500

    se sube repositario suscripciones

---
+++ Fin/WebAPIAutores/WebAPIAutores/Migrations/20220521050140_Llaves.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIAutores.Migrations
+{
+    public partial class Llaves : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LlavesAPI",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Llave = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoLlave = table.Column<int>(type: "int", nullable: false),
+                    Activa = table.Column<bool>(type: "bit", nullable: false),
+                    UsuarioId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LlavesAPI", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LlavesAPI");
+        }
+    }
+}
